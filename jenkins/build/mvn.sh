745275f0e75ba202a609698a3a87398f4a708879
shavinder <shavinder.g@gmail.com>
2020-05-27 19:38:43 +0530

    msg

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,6 @@ echo "*********************************"
 echo "building jar"
 echo "*********************************"
 WORKSPACE=/var/jenkins_home/workspace/pipelinetest
-docker run --rm -v $WORKSPACE/sample-app:/app -v /root/.m2 -w /app maven:3-alpine "$@"
+docker run -v $WORKSPACE/sample-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+#docker run --rm -v /var/jenkins_home/workspace/pipelinetest/sample-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine mvn -B -DskipTests clean package
+#docker run --rm -v /home/shav/Desktop/pipeline/sample-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine mvn -B -DskipTests clean package
